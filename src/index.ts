ec9c502a99ba21419e7ee538d8ef9a648531de55
Danil Ryzhov <daniil.ryzhov@vk.team>
2022-10-11 19:25:44 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import fs = require("fs-extra");
+import fs = require("fs");
 import path = require("path");
 
 const ROOT_DIR = path.resolve(__dirname, '..');
@@ -6,7 +6,7 @@ const ROOT_DIR = path.resolve(__dirname, '..');
 export const DIST_PATH = `${ROOT_DIR}/dist`;
 export const rmDist = (): void => {
     if (fs.existsSync(DIST_PATH)) {
-        fs.rmdirSync(DIST_PATH, {recursive: true});
+        fs.rmSync(DIST_PATH, { recursive: true });
     }
 }
 
